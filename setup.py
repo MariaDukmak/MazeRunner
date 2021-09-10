301a49da6005e9de88bfba641f3cdce8c0a36ea8
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-09-10 12:15:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+
+]
+
+setup(
+    name='fancymazerunner',
+    description='A fancymazerunner agent based simulation',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    python_requires='>=3.6',
+    author='Maria Dukmak',
+    author_email='Maria.dukmak@student.hu.nl',
+    keyword='agents', 'agent-based',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',,
+)
