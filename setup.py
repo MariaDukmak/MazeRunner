ebdfba803b4b936ce7e934ed7a5622d1fd73585b
Lucas van der Horst <Lucas@vdrHorst.nl>
2021-09-13 16:14:42 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     python_requires='>=3.6',
     author='Maria Dukmak',
     author_email='Maria.dukmak@student.hu.nl',
-    keyword='agents', 'agent-based',
+    keyword=['agents', 'agent-based'],
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',,
+    long_description_content_type='text/markdown',
 )
