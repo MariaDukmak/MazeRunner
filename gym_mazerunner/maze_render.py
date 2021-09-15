e869c4d19fc93ebaa39bd90b2dad89cfd8c06f17
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-09-15 11:31:52 +0200

    Added render location for all agents function and cleaned code
    Made with Jelle

--- gym_mazerunner/maze_render.py
+++ gym_mazerunner/maze_render.py
@@ -4,8 +4,10 @@ import os
 
 from PIL import Image
 
+import numpy as np
 
-def render_background(maze, save_background: bool = False, file_name: str = "maze.png"):
+
+def render_background(maze: np.array, save_background: bool = False, file_name: str = "maze.png") -> Image:
     """
     Render the maze and save it if specified.
 
@@ -39,8 +41,59 @@ def render_background(maze, save_background: bool = False, file_name: str = "maz
             os.makedirs("maze_output")
         background.save(os.path.join("maze_output", file_name))
 
+    return background
 
-if __name__ == '__main__':
+
+def render_agent_in_step(maze_information: np.array, background_image: Image, agent_location_list: [np.array],
+                         step: int = 0, want_to_save: bool = False) -> Image:
+    """
+    Render all agents from step.
+
+    :param maze_information: Information about the maze
+    :param background_image: Rendered background from maze information
+    :param agent_location_list: List of locations from all agents
+    :param step: Current step of the environment
+    :param want_to_save: Save current step to folder
+    :return:
+    """
+    # Copy from background
+    copy_background = background_image.copy()
+    # Declare with images used for the agent
+    agent_icon = Image.open("textures/agent.png")
+
+    # Calculate canvas size of the maze
+    tile_width = background_image.width // maze_information.shape[0]
+    tile_height = background_image.height // maze_information.shape[1]
+
+    for index, item in enumerate(agent_location_list):
+        agent_icon_copy = agent_icon.copy()
+
+        # TODO change each agent with idex variable
+
+        copy_background.paste(agent_icon_copy, (item[0] * tile_width, item[1] * tile_height), agent_icon)
+
+    if want_to_save:
+        if not os.path.exists("maze_output"):
+            os.makedirs("maze_output")
+        copy_background.save(os.path.join("maze_output", f"testsim-{step}.png"))
+
+    return copy_background
+
+
+def main():
+    """Test this class."""
     from maze_generator import generate_maze
 
-    render_background(generate_maze(), True)
+    maze_background = generate_maze()
+
+    rendered_background = render_background(maze_background, True)
+
+    agent_location_list = np.array([[8, 8], [9, 9], [16, 16], [0, 0]])
+
+    step = 42
+
+    render_agent_in_step(maze_background, rendered_background, agent_location_list, step, True)
+
+
+if __name__ == '__main__':
+    main()
