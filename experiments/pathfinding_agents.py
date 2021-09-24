5362ab78dbc951643f152637ac76efad5c6fd532
Richard <60653249+RichardDev01@users.noreply.github.com>
2021-09-24 13:49:09 +0200

    Boundary check function (#13)
    * Added code for boundary check
    
    * Fixed pathfinding bug

--- experiments/pathfinding_agents.py
+++ experiments/pathfinding_agents.py
@@ -4,7 +4,7 @@ from mazerunner_sim.policies import PathFindingPolicy
 from mazerunner_sim.envs import MazeRunnerEnv
 
 
-env = MazeRunnerEnv(n_runners=5, day_length=150, maze_size=20)
+env = MazeRunnerEnv(n_runners=5, day_length=300, maze_size=10)
 agents = [PathFindingPolicy() for _ in range(env.n_runners)]
 
-run_simulation(env, agents, wait_key=100, follow_runner_id=0)
+run_simulation(env, agents, wait_key=10, follow_runner_id=0)
