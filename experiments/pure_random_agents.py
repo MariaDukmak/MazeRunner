591387a5c110844f72ad5885cf5572b9cf08db34
MariaDukmak <maryadukmak.ho@gmail.com>
2021-09-24 13:58:07 +0200

    Deleted pseudocode :)

--- experiments/pure_random_agents.py
+++ experiments/pure_random_agents.py
@@ -3,9 +3,6 @@ from mazerunner_sim import run_simulation
 from mazerunner_sim.policies import PureRandomPolicy
 from mazerunner_sim.envs import MazeRunnerEnv
 
-def batchrunner(n_runners, day_lenth, percentage_policies: Tuple[float, float, float], n_simulations, ) -> None:
-    pass
-
 env = MazeRunnerEnv(n_runners=5, day_length=1000)
 agents = [PureRandomPolicy() for _ in range(env.n_runners)]
 
