5167bb15e2c61954bc7e4c5875a2884e58afcc8a
MariaDukmak <maryadukmak.ho@gmail.com>
2021-09-28 13:02:46 +0200

    Fixed flake8 errors.

--- experiments/2_pathfindinge_agents/experiment_script.py
+++ experiments/2_pathfindinge_agents/experiment_script.py
@@ -1,5 +1,4 @@
 """
-
 Run a batch experiment with two pathfinding runners,
 a day-length of 300,
 a maze-size of 10.
@@ -8,11 +7,16 @@ Run the simulation 10 times.
 from mazerunner_sim import run_batch
 from mazerunner_sim.policies import PathFindingPolicy
 from mazerunner_sim.envs import MazeRunnerEnv
+from mazerunner_sim.envs import Runner
 
+runners = [
+    Runner(action_speed=0),
+    Runner(action_speed=0),
+]
 policies = [
     PathFindingPolicy(),
     PathFindingPolicy(),
 ]
-env = MazeRunnerEnv(n_runners=2, day_length=300, maze_size=10)
+env = MazeRunnerEnv(runners=runners, day_length=300, maze_size=10)
 
 run_batch([env], policies, batch_size=10)
