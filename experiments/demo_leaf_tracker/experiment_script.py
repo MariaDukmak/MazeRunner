eca7988a7f1bf2873cb27e35a1f84c8a74c225a1
Lucas van der Horst <Lucas@vdrHorst.nl>
2021-09-29 20:56:37 +0200

    Changed leaf-tracker demo

--- experiments/demo_leaf_tracker/experiment_script.py
+++ experiments/demo_leaf_tracker/experiment_script.py
@@ -6,14 +6,18 @@ from mazerunner_sim.envs import MazeRunnerEnv, Runner
 
 
 runners = [
-    Runner(action_speed=1),
     Runner(action_speed=0),
+    Runner(action_speed=0),
+    Runner(action_speed=1),
+    Runner(action_speed=2),
 ]
 policies = [
     LeafTrackerPolicy(),
     LeafTrackerPolicy(),
+    LeafTrackerPolicy(),
+    LeafTrackerPolicy(),
 ]
 
-env = MazeRunnerEnv(runners, day_length=100)
+env = MazeRunnerEnv(runners, day_length=200, maze_size=26)
 
-run_simulation(env, policies, wait_key=10, follow_runner_id=0)
+run_simulation(env, policies, wait_key=1, follow_runner_id=0)
