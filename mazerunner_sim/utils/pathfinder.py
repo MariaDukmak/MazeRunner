d6f2b048982d7d48b31631f10cf3c723c72d5ea0
Richard <60653249+RichardDev01@users.noreply.github.com>
2021-09-29 16:09:57 +0200

    Memory decay (#17)
    * Remove weird git bug 🐛🐛🐛🐛🐛🐛🐛
    
    * Created Observation and Action classes,
    and changed the observation to use a memory map
    
    * Progress for Richard,
    
    I'm really sorry for this kind of commit message, but time
    
    * Created memory decay function.
    
    Created memory decay function placeholder.
    
    * First version memory decay.
    
    * Function works with little bug
    
    * Created basis for the LeafTrackerPolicy.
    In the process fixed the PathFindingPolicy and cleaned up some code.
    
    * Fixed runner dead isue, added time to sim window
    
    * Created new pathfinding function
    
    Created a new pathfinding function to fix a pathfinding bug. bugfixing still needed.
    
    * Added pathfinding support to memory decay
    
    * Corrected file for flake8
    
    * Changed sprite to fit format
    
    Co-authored-by: Lucas van der Horst <Lucas@vdrHorst.nl>
    Co-authored-by: Jellestiesri <jellestiesri@gmail.com>

--- mazerunner_sim/utils/pathfinder.py
+++ mazerunner_sim/utils/pathfinder.py
@@ -47,20 +47,66 @@ def traceback_visited(visited_tree: Dict[Coord, Coord], end: Coord, origin: Coor
     return path
 
 
-def check_boundary(maze: np.array, next_tile: np.array) -> bool:
+def coord_within_boundary(maze: np.array, coord: Coord) -> bool:
     """
-    Check if next tile lays within maze boundaries.
+    Check if the given coordinate lays within maze boundaries.
 
     :param maze: a 2D boolean array where True means walkable tile and False means a wall
-    :param next_tile: ...
+    :param coord: a 2D coordinate
     :return: false if out of bounds, true if inbounds
     """
-    boundery = range(0, maze.shape[0] - 1)
+    x, y = coord
+    height, width = maze.shape
+    return 0 <= x < width and 0 <= y < height
 
-    if not next_tile[0] in boundery or not next_tile[1] in boundery:
-        return False
-    else:
-        return True
+
+def surrounding_tiles(coord: Coord) -> List[Coord]:
+    """
+    Get surrounding tiles from giving tile.
+
+    :param coord: Given coord to check surrounding
+    :return: np array of surrounding_tiles
+    """
+    x, y = coord
+    surrounding = [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
+    return surrounding
+
+
+def compute_explore_paths(start_coord: Coord, runner_known_map: np.array, runner_explored_map: np.array):
+    """
+    Compute paths for exploring
+
+    :param start_coord: Start coords
+    :param runner_known_map: Known map of the runner
+    :param runner_explored_map: Explored map of the runner
+    :return:
+    """
+    edge_tiles = []
+    visited_tiles = {start_coord: None}  # key: next_tile, value: previous_tile
+
+    q = PriorityQueue()
+    q.put((0, start_coord))
+    while not q.empty():
+        # gets the tile with the lowest priority value
+        priority, tile = q.get()
+
+        for next_tile in surrounding_tiles(tile):
+            if coord_within_boundary(runner_known_map, next_tile):
+
+                # Check coords with maze if you can walk there
+                if runner_known_map[next_tile[1], next_tile[0]] and next_tile not in visited_tiles:
+
+                    x, y = next_tile[0] + 1, next_tile[1] + 1
+                    explored = np.pad(runner_explored_map, (1, 1), 'constant', constant_values=False)
+                    #     UP                          LEFT                   RIGHT                       DOWN
+                    if not(explored[y - 1, x] and explored[y, x - 1] and explored[y, x + 1] and explored[y + 1, x]):
+                        edge_tiles.append(next_tile)
+                    q.put((priority + 1, next_tile))
+
+                    visited_tiles[next_tile] = tile
+
+    paths = [traceback_visited(visited_tiles, edge_tile, start_coord) for edge_tile in edge_tiles]
+    return paths
 
 
 def paths_origin_targets(origin: Coord, targets: List[Coord], maze: np.array) -> List[List[Coord]]:
@@ -94,17 +140,14 @@ def paths_origin_targets(origin: Coord, targets: List[Coord], maze: np.array) ->
     while len(targets_togo) > 1:
         priority, tile = q.get()
 
-        if not check_boundary(maze, tile):
-            continue
-
         for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
             next_tile = tile[0] + dx, tile[1] + dy
-
-            if maze[next_tile[1], next_tile[0]] and next_tile not in visited_tree:
-                q.put((priority + 1, next_tile))
-                visited_tree[next_tile] = tile
-                if next_tile in targets_togo:
-                    targets_togo.remove(next_tile)
+            if coord_within_boundary(maze, next_tile):
+                if maze[next_tile[1], next_tile[0]] and (next_tile not in visited_tree):
+                    q.put((priority + 1, next_tile))
+                    visited_tree[next_tile] = tile
+                    if next_tile in targets_togo:
+                        targets_togo.remove(next_tile)
 
     if len(targets_togo) == 1:
         done = False
@@ -117,17 +160,15 @@ def paths_origin_targets(origin: Coord, targets: List[Coord], maze: np.array) ->
     while not done:
         _, tile = q.get()
 
-        if not check_boundary(maze, tile):
-            continue
-
         for dx, dy in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
             next_tile = tile[0] + dx, tile[1] + dy
-            if maze[next_tile[1], next_tile[0]] and next_tile not in visited_tree:
-                q.put((distance_func(next_tile), next_tile))
-                visited_tree[next_tile] = tile
-                if next_tile in targets_togo:
-                    done = True
-                    break
+            if coord_within_boundary(maze, next_tile):
+                if maze[next_tile[1], next_tile[0]] and (next_tile not in visited_tree):
+                    q.put((distance_func(next_tile), next_tile))
+                    visited_tree[next_tile] = tile
+                    if next_tile in targets_togo:
+                        done = True
+                        break
 
     # Trace back the path for each of the targets
     return [traceback_visited(visited_tree, target, origin) for target in targets]
