2ee8b3fc6b29cdb7ba2378e7d9776de9a287ec42
MariaDukmak <maryadukmak.ho@gmail.com>
2021-10-10 14:30:56 +0200

    Changes to the batch_runner.py

--- experiments/speed_comparison/experiment_script.py
+++ experiments/speed_comparison/experiment_script.py
@@ -2,18 +2,9 @@ from typing import Union
 from mazerunner_sim import BatchRunner, HiddenState
 
 
-class CustomBatchRunner(BatchRunner):
-    def __init__(self, filename: str):
-        super().__init__(filename)
+if __name__ == '__main_':
 
-    def update(env, data: Union[HiddenState, None]) -> HiddenState:
-        # stuff
-        pass
+    batch_runner = CustomBatchRunner('snelheid_test.feather')
+    batch_runner.run_batch(envs=[env], policies=policies, batch_size=10)
 
-    def finish(env, data: HiddenState) -> dict:
-        # stuff
-        pass
-
-
-batch_runner = CustomBatchRunner()
-batch_runner.run_batch()
+    print(feather.read_feather('snelheid_test.feather'))
